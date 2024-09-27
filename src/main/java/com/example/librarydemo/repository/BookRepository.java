46bb1d082d4328c3b5f22ed4c02fb5597d0df0cf
huiconglai <huicong@snappymob.com>
2024-09-27 15:15:33 +0800

    Add Book and Borrower repository

---
+++ src/main/java/com/example/librarydemo/repository/BookRepository.java
@@ -0,0 +1,9 @@
+package com.example.librarydemo.repository;
+
+import com.example.librarydemo.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
